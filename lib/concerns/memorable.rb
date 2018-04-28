b6e076b44437b0e34c4d0cc1c4302b36dab3e2bf
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-28 00:39:56 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -10,8 +10,8 @@ module Memorable
   end
 
   module InstanceMethods
-    #def initialize
-    #  self.class.all << self
-    #end
+    def initialize
+      self.class.all << self
+    end
   end
 end
